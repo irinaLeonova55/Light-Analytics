9f09ae78a4f638b500a619542fa2d706891c2f39
Irina Leonova <irinaleonova1988@gmail.com>
2026-04-03 21:36:47 +0300

    added Sellers section

--- src/widgets/sellers/ui/Sellers.jsx
+++ src/widgets/sellers/ui/Sellers.jsx
@@ -4,6 +4,9 @@ import Headline from '@/shared/ui/Headline/Headline';
 
 import sellersArrow from '@/shared/assets/icons/sellers-arrow.svg';
 import sellersBgUp from '@/shared/assets/images/sellers-bg-up.png';
+import sellersFigure from '@/shared/assets/images/sellers-figure.png';
+import sellersBgDown from '@/shared/assets/images/sellers-bg-down.png';
+import sellersTriangle from '@/shared/assets/images/sellers-triangle.png';
 
 const Sellers = () => {
   const advantages = [
@@ -45,6 +48,7 @@ const Sellers = () => {
               backgroundImage: `url(${sellersBgUp})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
+              position: 'relative',
             }}
             className={styles.sellerImgUp}
           >
@@ -59,6 +63,48 @@ const Sellers = () => {
                 </div>
               ))}
             </div>
+            <img
+              src={sellersFigure}
+              alt="Фигура"
+              className={styles.sellersFigure}
+            />
+          </div>
+
+          <div
+            style={{
+              backgroundImage: `url(${sellersBgDown})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+            className={styles.sellerImgDown}
+          >
+            <div className={styles.triangleWrapper}>
+              <span className={styles.accuracy}>Точность</span>
+              <img
+                src={sellersTriangle}
+                alt="Треугольник"
+                loading="lazy"
+                className={styles.triangle}
+              />
+              <span className={styles.control}>Контроль</span>
+              <span className={styles.speed}>Скорость</span>
+            </div>
+          </div>
+
+          <div className={styles.sellersItemText}>
+            <img
+              src={sellersArrow}
+              alt="Стрелка"
+              className={styles.sellersImg}
+            />
+            <div className={styles.sellersTitle}>
+              Видите чистую прибыль по каждому товару каждый день
+            </div>
+            <p className={styles.sellersDesc}>
+              Не оборот, не “грязную” маржу, а реальную чистую прибыль с учетом
+              комиссий, логистики, хранения и всех дополнительных расходов.
+              Решения принимаются на основе точных цифр, а не догадок
+            </p>
           </div>
         </div>
       </Container>
